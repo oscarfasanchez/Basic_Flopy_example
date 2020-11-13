b6050e79f2e7300dbc03f89b73657c860737ec32
Oscar Sanchez <oscar.sanchez2@correo.uis.edu.co>
2020-11-13 16:33:41 -0500

    este es el primer commit solo con los parametros iniciales y la versión inicial2

--- Flopymf6example.py
+++ Flopymf6example.py
@@ -12,4 +12,15 @@ import flopy
 
 
 #parameters
-name= "tutorial01_mf6"
+name = "tutorial01_mf6"
+h1 = 100
+h2 = 90
+Nlay = 10 
+N = 101
+L = 400
+H = 50
+k = 1.0
+
+sim = flopy.mf6.MFSimulation(
+    sim_name = name, exe_name="mf6", version="mf6",
+    sim_ws="C:/WRDAPP/mf6.2.0/bin")
