05aeb01c47502172be170544f75e3ec2f85fdd8d
Oscar Sanchez <oscar.sanchez2@correo.uis.edu.co>
2020-11-13 16:32:22 -0500

    este es el primer commit solo con los parametros iniciales y la versión inicial

---
+++ Flopymf6example.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 13 08:30:24 2020
+
+@author: Oscar
+"""
+
+import os 
+import numpy as np
+import matplotlib.pyplot as plt
+import flopy
+
+
+#parameters
+name= "tutorial01_mf6"
