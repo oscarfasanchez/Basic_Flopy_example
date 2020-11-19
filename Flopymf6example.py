333a774cc3cd204d6607cd06197d7fd899809c64
Oscar Sanchez <oscar.sanchez2@correo.uis.edu.co>
2020-11-19 11:52:12 -0500

    incluí condiciones inciales y paquete de flujo

--- Flopymf6example.py
+++ Flopymf6example.py
@@ -32,3 +32,38 @@ sim = flopy.mf6.MFSimulation(
 tdis = flopy.mf6.ModflowTdis(sim, pname="tdis",
                              time_units="DAYS"
                              , nper=1, perioddata=[(1,1,1)])
+#creating the solver package
+
+ims = flopy.mf6.ModflowIms(sim,
+                           pname= "ims", complexity="SIMPLE")
+
+
+#creating the groundwater model
+model_nam_file = f'{name}.nam'
+gwf = flopy.mf6.ModflowGwf(sim, modelname=model_nam_file)
+
+
+# now we have to define the entire groundwater model
+
+# Creating the discretization package
+
+bot= np.linspace(-H/Nlay, -H, Nlay)
+delrow = delcol = L / ( N - 1 )
+dis = flopy.mf6.ModflowGwfdis(gwf, nlay=Nlay, nrow=N, ncol=N, 
+                              delr=delrow, delc=delcol, top=0.0, 
+                              botm=bot)
+# Creating initial conditions
+
+start = h1 * np.ones((Nlay, N, N))
+ic = flopy.mf6.ModflowGwfic(gwf, pname="ic", strt=start )
+
+
+# celltype 1 are unconfined thickness
+flopy.mf6.ModflowGwfnpf( gwf, icelltype=1, k=k, save_flows=True)
+
+
+
+
+
+
+ic = flopy.mf6.ModflowGwfnpf(gwf, icelltype=1, k=k, save_flows=True)
