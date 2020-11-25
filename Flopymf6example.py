98d2f536e8fcd4c06eef4604e9cfb9146750b8ff
Oscar Sanchez <oscar.sanchez2@correo.uis.edu.co>
2020-11-25 11:28:46 -0500

    primer grafico perfil para experimentos

--- Flopymf6example.py
+++ Flopymf6example.py
@@ -12,13 +12,18 @@ import flopy
 
 
 #parameters
+
 name = "tuto01_mf6"
+# hidraulic heads
 h1 = 100
 h2 = 90
+# number of layers
 Nlay = 10 
+# Mesh dimensions
 N = 101
 L = 400
 H = 50
+# number of stress periods
 k = 1.0
 
 
@@ -68,14 +73,14 @@ flopy.mf6.ModflowGwfnpf( gwf, icelltype=1, k=k,
 # Creating constat head package
 
 chd_rec=[]
-chd_rec.append(((0 , int(N / 4),int(N / 4)), h2))
+chd_rec.append(((5 , int(3*N / 4),int(N / 4)), h2))
 for layer in range(0, Nlay):
     for row_col in range(0, N):
         chd_rec.append(((layer, row_col, 0), h1))
         chd_rec.append(((layer, row_col, N - 1), h1))
         if row_col !=0 and row_col!= N - 1:
-            chd_rec.append(((layer, 0, row_col), h1))
-            chd_rec.append(((layer, N - 1, row_col), h1))
+            chd_rec.append(((layer, 0, row_col), h2))
+            chd_rec.append(((layer, N - 1, row_col), h2))
 
 chd = flopy.mf6.ModflowGwfchd(gwf, maxbound=len(chd_rec), 
                               stress_period_data=chd_rec,
@@ -110,3 +115,28 @@ sim.write_simulation()
 success, buff= sim.run_simulation()
 if not success:
     raise Exception("MODFLOW 6 did not terminate normally.")
+    
+# Plot map layer 1
+# import results
+hds = flopy.utils.binaryfile.HeadFile(headfile)
+# Take from first stress period
+h = hds.get_data(kstpkper=(0,0))
+# create the mesh for the graph
+x = y = np.linspace(0 , L, N)
+fig = plt.figure(figsize=(6,6))
+ax=fig.add_subplot(1,1,1, aspect="equal")
+c=ax.contour(x, y, h[0], np.arange(90, 100.1, 0.2), colors="black")
+plt.clabel(c, fmt="%2.1f")
+
+
+# plot a cross section along a row
+# z = np.linspace(-H / Nlay /2, -H + H/ Nlay / 2,  Nlay)
+z = np.linspace(-H/Nlay, -H,  Nlay) #prueba oscar
+fig = plt.figure(figsize=(5, 2.5))
+ax = fig.add_subplot(1, 1, 1, aspect="auto")
+c = ax.contour(x, z , h[:, 30, :], np.arange(0, 100, 0.2), colors="black")#take care about the cell number, not distance!
+plt.clabel(c, fmt="%1.1f")
+
+
+   
+    
